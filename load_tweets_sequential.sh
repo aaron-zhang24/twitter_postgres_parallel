860c81fd25b169d6553b133755434e0e83cf3317
aaron-zhang24 <aazhang@g.hmc.edu>
2026-04-14 17:02:57 -0700

    copied load_tweets.py from twitter_postgres and modified load_tweets_sequential by copying from load_tweets.sh

--- load_tweets_sequential.sh
+++ load_tweets_sequential.sh
@@ -8,6 +8,7 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
+    unzip -p "$file" | sed 's/\\u0000//g' | psql postgresql://postgres:pass@localhost:1108 -c "COPY tweets_jsonb (data) FROM STDIN csv quote e'\x01' delimiter e'\x02';"
 done
 
 echo '================================================================================'
@@ -16,11 +17,12 @@ echo '==========================================================================
 time for file in $files; do
     echo
     # copy your solution to the twitter_postgres assignment here
+    python3 load_tweets.py --inputs "$file" --db postgresql://postgres:pass@localhost:11108
 done
 
 echo '================================================================================'
 echo 'load pg_normalized_batch'
 echo '================================================================================'
 time for file in $files; do
-    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:3/ --inputs $file
+    python3 -u load_tweets_batch.py --db=postgresql://postgres:pass@localhost:11111 --inputs $file
 done
